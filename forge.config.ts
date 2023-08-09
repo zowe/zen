dc276a6e13b7ae303fa1fb6629e4e3b4c6db2f56
James Struga <jstruga@rocketsoftware.com>
2023-08-08 21:34:31 -0400

    add maker dmg
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>

--- forge.config.ts
+++ forge.config.ts
@@ -10,6 +10,7 @@
 
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
@@ -21,7 +22,7 @@ import { rendererConfig } from './configs/webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {},
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel({}), new MakerDMG({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
   plugins: [
     new WebpackPlugin({
       mainConfig,
