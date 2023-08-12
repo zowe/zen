0ab493f3d3d8d08d5a4994eb4bd22beeb5a976b3
Sakshi Bobade <bobadesakshi21@gmail.com>
2023-08-12 22:36:06 +0530

    Updating styles for the form

--- src/renderer/components/common/JsonForms.tsx
+++ src/renderer/components/common/JsonForms.tsx
@@ -14,6 +14,74 @@ import {
   materialRenderers,
   materialCells,
 } from '@jsonforms/material-renderers';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const customTheme = createTheme({
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          display: 'inline-flex', // Display the input field inline
+          alignItems: 'center',   // Align items vertically in the input container
+        },
+        fullWidth: {
+          width: '70%',
+          // width: '50%', // Set your desired custom width here
+        },
+      },
+    },
+    MuiGrid: {
+      styleOverrides: {
+        root: {
+          boxShadow: 'none',
+          borderRadius: 0,
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          boxShadow: 'none',
+          borderRadius: 0,
+          marginLeft: '-20px',
+          background: 'none',
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          boxShadow: 'none',
+          borderRadius: 0,
+        },
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          verticalAlign: 'middle'
+        },
+      },
+    },
+    MuiCardHeader: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'Courier New, monospace',
+          fontSize: '5px',
+          marginTop: '15px',
+          marginBottom: '-20px',
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          width: '30%',
+        },
+      },
+    },
+  },
+});
 
 // REVIEW: Flatten the schema UI or find out how to keep structure
 
@@ -29,12 +97,29 @@ const makeUISchema = (schema: any, base: string): any => {
         scope: `#/properties${base}${p}/properties/${sp}`,
       }));
 
+      const groupedControls = [];
+      let row = [];
+      for (let i = 0; i < subElements.length; i++) {
+        row.push(subElements[i]);
+        if (row.length === 2 || (row.length === 1 && i === subElements.length - 1)) {
+          groupedControls.push({
+            type: 'HorizontalLayout',
+            elements: row,
+          });
+          row = [];
+        }
+      }
+
       return {
         type: 'Group',
-        label: p,
-        elements: subElements,
+        label: `\n${p}:`,
+        elements: [
+          {
+            type: 'VerticalLayout', // Use HorizontalLayout for controls within the group
+            elements: groupedControls,
+          },
+        ],
       };
-      // return makeUISchema(schema.properties[p], `${base}${p}/properties/`);
     } else {
       return {
         "type": "Control",
@@ -52,6 +137,7 @@ export default function JsonForm(props: any) {
   const {schema, initialdata, onChange} = props;
   // const [data, setData] = useState(initialdata);
   return (
+    <ThemeProvider theme={customTheme}>
     <JsonForms
       schema={schema}
       uischema={makeUISchema(schema, '/')}
@@ -65,5 +151,6 @@ export default function JsonForm(props: any) {
         onChange(data);
       }}
     />
+    </ThemeProvider>
   );
-  }
+}
