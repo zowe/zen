532ad4aa02ff2fd4b68995bf5e80c7a1ec006565
Timothy Gerstel <tim.gerstel@gmail.com>
2023-09-18 16:06:44 -0400

    Add ipcMain hook to check if dir exists from UI
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- src/renderer/preload.ts
+++ src/renderer/preload.ts
@@ -55,6 +55,9 @@ contextBridge.exposeInMainWorld('electron', {
     checkSpace(connectionArgs: IIpcConnectionArgs, location: string) {
       return ipcRenderer.invoke("check-space", connectionArgs, location);
     },
+    checkDirExists(connectionArgs: IIpcConnectionArgs, location: string) {
+      return ipcRenderer.invoke("check-dir-exists", connectionArgs, location);
+    },
     installButtonOnClick(connectionArgs: IIpcConnectionArgs, installationArgs: {installDir: string, javaHome: string, nodeHome: string, installationType: string, userUploadedPaxPath: string}, version: string) {
       return ipcRenderer.invoke("install-mvs", connectionArgs, installationArgs, version);
     },
