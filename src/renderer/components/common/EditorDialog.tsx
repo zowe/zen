47fd7ce3bdface2b5a123914dfb0f8ef03369772
Sakshi Bobade <bobadesakshi21@gmail.com>
2023-10-16 19:51:57 +0530

    Using the editor for JCL and output of JCL

--- src/renderer/components/common/EditorDialog.tsx
+++ src/renderer/components/common/EditorDialog.tsx
@@ -17,7 +17,23 @@ import { setConfiguration, setZoweConfig, getZoweConfig } from '../../../service
 import Ajv2019 from "ajv/dist/2019"
 import MonacoEditorComponent from "../common/MonacoEditor";
 
-const EditorDialog = ({isEditorVisible, toggleEditorVisibility, onChange} : any) => {
+const test_jcl = `
+//MYJOB   JOB (ACCT), 'My Job Description',
+//         CLASS=A,MSGCLASS=X,NOTIFY=&SYSUID
+
+//* This is a comment
+
+//STEP1   EXEC PGM=MYCOBOL
+//INPUT   DD  DSN=your_input_dataset,DISP=SHR
+//OUTPUT  DD  DSN=your_output_dataset,DISP=(,CATLG,DELETE)
+//SYSOUT  DD  SYSOUT=A
+`;
+
+const test_op = "TEST OUTPUT";
+
+const EditorDialog = (
+  {contentType, isEditorVisible, toggleEditorVisibility, onChange} : 
+  {contentType: any, isEditorVisible: boolean, toggleEditorVisibility: any, onChange?: any}) => {
 
   const dispatch = useAppDispatch();
   const schema = useAppSelector(selectSchema);
@@ -28,23 +44,32 @@ const EditorDialog = ({isEditorVisible, toggleEditorVisibility, onChange} : any)
   const [isSchemaValid, setIsSchemaValid] = useState(true);
   const [schemaError, setSchemaError] = useState('');
   const fileInputRef = useRef(null);
+  let initZoweConfig: any;
 
-  const ajv = new Ajv2019()
-  ajv.addKeyword("$anchor");
-  const draft7MetaSchema = require("ajv/dist/refs/json-schema-draft-07.json")
-  ajv.addMetaSchema(draft7MetaSchema)
-  const validate = ajv.compile(schema);
-
-  const initZoweConfig = getZoweConfig();
+  if(contentType == 'yaml') {
+    initZoweConfig = getZoweConfig();
+  }
 
   useEffect(() => {
     setEditorVisible(isEditorVisible);
     if(isEditorVisible) {
-      setEditorContent(dump(initZoweConfig));
+      if(contentType == 'yaml') {
+        setEditorContent(dump(initZoweConfig));
+      }
+      if(contentType == 'jcl') {
+        setEditorContent(test_jcl);
+      }
+      if(contentType == 'output') {
+        setEditorContent(test_op);
+      }
     }
   }, [isEditorVisible])
 
+
   const handleEditorContentChange = (newCode: any, isError: boolean) => {
+    if(contentType !== 'yaml') {
+      return;
+    }
     if(isError) {
       dispatch(setNextStepEnabled(false));
       return;
@@ -66,6 +91,11 @@ const EditorDialog = ({isEditorVisible, toggleEditorVisibility, onChange} : any)
     }
 
     // To validate the javascript object against the schema
+    const ajv = new Ajv2019()
+    ajv.addKeyword("$anchor");
+    const draft7MetaSchema = require("ajv/dist/refs/json-schema-draft-07.json")
+    ajv.addMetaSchema(draft7MetaSchema)
+    const validate = ajv.compile(schema);
     const isValid = validate(jsonData);
     setIsSchemaValid(isValid);
 
@@ -141,9 +171,9 @@ const EditorDialog = ({isEditorVisible, toggleEditorVisibility, onChange} : any)
             width: '100vw',
           },
         }}>
-        <DialogTitle>zowe.yaml</DialogTitle>
+        <DialogTitle>Editor</DialogTitle>
         <DialogContent sx={{paddingBottom: '0'}}>
-          <MonacoEditorComponent initialContent={editorContent} onContentChange={handleEditorContentChange} isSchemaValid={isSchemaValid} schemaError={schemaError} />
+          <MonacoEditorComponent contentType={contentType} initialContent={editorContent} onContentChange={handleEditorContentChange} isSchemaValid={isSchemaValid} schemaError={schemaError} />
         </DialogContent>
         <DialogActions>
           <Button onClick={triggerFileInputClick}>Import</Button>
--- src/renderer/components/common/MonacoEditor.tsx
+++ src/renderer/components/common/MonacoEditor.tsx
@@ -3,48 +3,107 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import { load } from 'js-yaml';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { JCL_HILITE } from '../../monaco/highlighters/jcl';
+import { YML_HILITE } from '../../monaco/highlighters/yaml';
 
-const MonacoEditorComponent = ({initialContent, onContentChange, isSchemaValid, schemaError} : any) => {
+const JCL_LANG = {
+  id: 'jcl',
+  extensions: ['.jcl', '.cntl'],
+  filenamePatterns: ['\\.jcl\\.','\\.jcl','\\.cntl','\\.cntl\\.'],
+  aliases: ['JCL', 'jcl'],
+  mimetypes: ['application/jcl']
+};
+
+const YAML_LANG = {
+  id: 'yaml',
+  extensions: ['.yaml', '.yml'],
+  filenamePatterns: ['\\.yaml\\.','\\.yaml','\\.yml','\\.yml\\.'],
+  aliases: ['YAML', 'yml'],
+  mimetypes: ['application/yml']
+};
+
+const MonacoEditorComponent = (
+  {contentType, initialContent, onContentChange, isSchemaValid, schemaError} : 
+  {contentType:string, initialContent: any, onContentChange?: any, isSchemaValid?: boolean, schemaError?: any}) => {
 
   const editorRef = useRef(null);
   const [isError, setIsError] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
 
+  let readOnly = false;
+  let lang = 'yaml';
+
   useEffect(() => {
-    monaco.languages.register({ id: 'yaml' });
-    monaco.languages.setMonarchTokensProvider('yaml', {
-      tokenizer: {
-        root: [
-          //Defining syntax highlighting rules here
-          [/^(\s*)([a-zA-Z_][\w]*)/, ['white', 'key']],// Unquoted keys
-          [/\b(true|false)\b/, 'keyword'], // Boolaean values
-          [/"[^"\\]*"/, 'string'], // Values enclosed in double quotes
-          [/'[^'\\]*'/, 'string'], // Values enclosed in single quotes
-          [/#.*$/, 'comment'], // Comments
-        ],
-      },
-    });
+    if(contentType == 'jcl') {
+      readOnly = true;
+      lang = 'jcl';
+
+      monaco.languages.register(JCL_LANG);
+      monaco.languages.setMonarchTokensProvider('jcl', {
+        tokenizer: {
+          root: [
+            [/^\/\/\*.*$/, 'jcl-comment'], // JCL comments
+            [/^[A-Z][A-Z0-9]*\s/, 'jcl-statement'], // JCL statements
+            [/^[A-Z][A-Z0-9]*$/, 'jcl-statement'], // JCL statements at the end of a line
+            [/[A-Z][A-Z0-9]*/, 'jcl-keyword'], // JCL keywords
+            [/\'.*?'/, 'jcl-string'], // Single-quoted strings
+            [/"(\\.|[^"])*"/, 'jcl-string'], // Double-quoted strings
+            [/\d+/, 'jcl-number'], // Numeric values
+            [/[,=]/, 'jcl-delimiter'], // Commas and equal signs
+            [/\b(ADD|SUB|MULT|DIV)\b/, 'jcl-math-operator'], // Math operators
+            [/\b(COPY|DELETE|MOVE)\b/, 'jcl-action'], // Action keywords
+            [/\b(JOB|EXEC|PRTY)\b/, 'jcl-parameters'], // Parameter keywords
+            [/\b(Abend|Cond|End|Exec|Ignore)\b/, 'jcl-event'], // Event keywords
+            [/\b(IF|THEN|ELSE|ENDIF)\b/, 'jcl-conditional'], // Conditional keywords
+            [/\b(AND|OR|NOT)\b/, 'jcl-logical'], // Logical operators
+            [/\b(SET|UNSET)\b/, 'jcl-setting'], // Setting keywords
+            [/\b(OPEN|CLOSE|READ|WRITE|DELETE)\b/, 'jcl-io'], // I/O operation keywords
+            [/\b(ERROR|INFO|WARNING|DEBUG)\b/, 'jcl-log-level'], // Log level keywords
+          ],
+        },
+      });
+    } else if(contentType == 'output') {
+      readOnly = true;
+      lang = 'plaintext';
+    }else if(contentType == 'yaml') {
+      monaco.languages.register(YAML_LANG);
+      monaco.languages.setMonarchTokensProvider('yaml', {
+        tokenizer: {
+          root: [
+            //Defining syntax highlighting rules here
+            [/^(\s*)([a-zA-Z_][\w]*)/, ['white', 'key']],// Unquoted keys
+            [/\b(true|false)\b/, 'keyword'], // Boolaean values
+            [/"[^"\\]*"/, 'string'], // Values enclosed in double quotes
+            [/'[^'\\]*'/, 'string'], // Values enclosed in single quotes
+            [/#.*$/, 'comment'], // Comments
+          ],
+        },
+      });
+    }
+
     editorRef.current = monaco.editor.create(document.getElementById('monaco-editor-container'), {
-      language: 'yaml', 
+      language: lang,
       theme: 'light',
       value: initialContent,
+      readOnly: readOnly
     });
 
     editorRef.current.onDidChangeModelContent(() => {
       const code = editorRef.current.getValue();
 
-      try {
-        // To parse the yaml and check if it is valid
-        const parsedYAML = load(code);
-        setError(false, '');
-        onContentChange(code, false);
-      } catch(error) {
-        let errorDesc = error.message ? error.message : "Invalid Yaml";
-        let errorMsg = error.reason ? error.reason : errorDesc;
-        setError(true, errorMsg);
-        onContentChange(code, true);
+      if(contentType == 'yaml') {
+        try {
+          // To parse the yaml and check if it is valid
+          const parsedYAML = load(code);
+          setError(false, '');
+          onContentChange(code, false);
+        } catch(error) {
+          let errorDesc = error.message ? error.message : "Invalid Yaml";
+          let errorMsg = error.reason ? error.reason : errorDesc;
+          setError(true, errorMsg);
+          onContentChange(code, true);
+        }
       }
-
     });
 
     return () => {
