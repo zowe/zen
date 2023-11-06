33cb126ebe5168d8999d20992e6dfdf5f7b55554
Timothy Gerstel <tim.gerstel@gmail.com>
2023-11-06 11:27:44 -0500

    Add new stage
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- src/renderer/components/configuration-wizard/Wizard.tsx
+++ src/renderer/components/configuration-wizard/Wizard.tsx
@@ -19,15 +19,17 @@ import spock from '../../assets/spock.svg'
 import InstallationType from '../stages/installation/InstallTypeSelection';
 import { selectLoading } from './wizardSlice';
 import { useAppSelector } from '../../hooks';
+import InitApfAuth from '../stages/InitApfAuth';
 
 const stages = [
   {id: 0, label: 'Connection', component: <Connection/>, nextButton: 'Continue'},
   {id: 1, label: 'Planning', component: <Planning/>, nextButton: 'Continue to installation options'},
   {id: 2, label: 'Installation Type', component: <InstallationType/>, nextButton: 'Continue to components installation'},
-  {id: 3, label: 'Installation', component: <Installation/>, nextButton: 'Continue to zowe Security'},
-  {id: 4, label: 'Security', component: <Security/>, nextButton: 'Continue to certificates setup'},
-  {id: 5, label: 'Certificates', component: <Certificates/>, nextButton: 'Continue to instance setup'},
-  {id: 6, label: 'Initialization', component: <Initialization/>, nextButton: <div style={{display: 'flex', alignItems: 'center'}}><img style={{width: '18px', height: '18px', paddingRight: '12px'}} src={spock}/>Live long and prosper</div>},
+  {id: 3, label: 'Installation', component: <Installation/>, nextButton: 'Continue to APF authorization'},
+  {id: 4, label: 'Initialize APF Auth', component: <InitApfAuth/>, nextButton: 'Continue to zowe Security'},
+  {id: 5, label: 'Security', component: <Security/>, nextButton: 'Continue to certificates setup'},
+  {id: 6, label: 'Certificates', component: <Certificates/>, nextButton: 'Continue to instance setup'},
+  {id: 7, label: 'Initialization', component: <Initialization/>, nextButton: <div style={{display: 'flex', alignItems: 'center'}}><img style={{width: '18px', height: '18px', paddingRight: '12px'}} src={spock}/>Live long and prosper</div>},
 ]
 
 const Wizard = () => {
