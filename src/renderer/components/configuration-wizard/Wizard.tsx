0960145e6ea97f2529b34d785426acf59439409a
Timothy Gerstel <tim.gerstel@gmail.com>
2023-12-19 14:16:26 -0500

    Move installation, security, and certificates stages to bustages of the initialization stage
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- src/renderer/components/configuration-wizard/Wizard.tsx
+++ src/renderer/components/configuration-wizard/Wizard.tsx
@@ -24,10 +24,14 @@ const stages = [
   {id: 0, label: 'Connection', component: <Connection/>, nextButton: 'Continue'},
   {id: 1, label: 'Planning', component: <Planning/>, nextButton: 'Continue to installation options'},
   {id: 2, label: 'Installation Type', component: <InstallationType/>, nextButton: 'Continue to components installation'},
-  {id: 3, label: 'Installation', component: <Installation/>, nextButton: 'Continue to zowe Security'},
-  {id: 4, label: 'Security', component: <Security/>, nextButton: 'Continue to certificates setup'},
-  {id: 5, label: 'Certificates', component: <Certificates/>, nextButton: 'Continue to instance setup'},
-  {id: 6, label: 'Initialization', component: <Initialization/>, nextButton: <div style={{display: 'flex', alignItems: 'center'}}><img style={{width: '18px', height: '18px', paddingRight: '12px'}} src={spock}/>Live long and prosper</div>},
+  // {id: 3, label: 'Installation', component: <Installation/>, nextButton: 'Continue to zowe Security'},
+  // {id: 4, label: 'Security', component: <Security/>, nextButton: 'Continue to certificates setup'},
+  // {id: 5, label: 'Certificates', component: <Certificates/>, nextButton: 'Continue to instance setup'},
+  {id: 6, label: 'Initialization', component: <Initialization/>, subStages: [
+    {id: 0, label: 'Installation', component: <Installation/>, nextButton: 'Continue to security setup'},
+    {id: 1, label: 'Security', component: <Security/>, nextButton: 'Continue to certificates setup'},
+    {id: 2, label: 'Certificates', component: <Certificates/>, nextButton: 'Continue to instance setup'},
+  ], nextButton: <div style={{display: 'flex', alignItems: 'center'}}><img style={{width: '18px', height: '18px', paddingRight: '12px'}} src={spock}/>Live long and prosper</div>},
 ]
 
 const Wizard = () => {
