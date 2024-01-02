918238f5a467dcbdfceee352268a429366dc091d
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-01-02 14:32:15 +0530

    final commit

--- src/renderer/components/configuration-wizard/Wizard.tsx
+++ src/renderer/components/configuration-wizard/Wizard.tsx
@@ -21,16 +21,13 @@ import { selectLoading } from './wizardSlice';
 import { useAppSelector } from '../../hooks';
 
 const stages = [
-  {id: 0, label: 'Connection', component: <Connection/>, nextButton: 'Continue'},
-  {id: 1, label: 'Planning', component: <Planning/>, nextButton: 'Continue to installation options'},
-  {id: 2, label: 'Installation Type', component: <InstallationType/>, nextButton: 'Continue to components installation'},
-  // {id: 3, label: 'Installation', component: <Installation/>, nextButton: 'Continue to zowe Security'},
-  // {id: 4, label: 'Security', component: <Security/>, nextButton: 'Continue to certificates setup'},
-  // {id: 5, label: 'Certificates', component: <Certificates/>, nextButton: 'Continue to instance setup'},
-  {id: 6, label: 'Initialization', component: <Initialization/>, subStages: [
-    {id: 0, label: 'Installation', component: <Installation/>, nextButton: 'Continue to security setup'},
-    {id: 1, label: 'Security', component: <Security/>, nextButton: 'Continue to certificates setup'},
-    {id: 2, label: 'Certificates', component: <Certificates/>, nextButton: 'Continue to instance setup'},
+  {id: 0, label: 'Connection', component: <Connection/>, hasJCL: false, isSkippable: false, hasOutput: false, steps: 1, nextButton: 'Continue'},
+  {id: 1, label: 'Planning', component: <Planning/>, hasJCL: false, isSkippable: false, hasOutput: true, steps: 3, nextButton: 'Continue to installation options'},
+  {id: 2, label: 'Installation Type', component: <InstallationType/>, hasJCL: false, isSkippable: false, hasOutput: false, steps: 1, nextButton: 'Continue to components installation'},
+  {id: 3, label: 'Initialization', component: <Initialization/>, hasJCL: true, isSkippable: true, hasYaml: true, hasOutput: true, steps: 1, subStages: [
+    {id: 0, label: 'Installation', component: <Installation/>, hasJCL: true, isSkippable: true, hasYaml: true, hasOutput: true, steps: 1, nextButton: 'Continue to security setup'},
+    {id: 1, label: 'Security', component: <Security/>, hasJCL: true, isSkippable: true, hasYaml: true, hasOutput: true, steps: 1, nextButton: 'Continue to certificates setup'},
+    {id: 2, label: 'Certificates', component: <Certificates/>, hasJCL: true, isSkippable: true, hasYaml: true, hasOutput: true, steps: 1, nextButton: 'Continue to instance setup'},
   ], nextButton: <div style={{display: 'flex', alignItems: 'center'}}><img style={{width: '18px', height: '18px', paddingRight: '12px'}} src={spock}/>Live long and prosper</div>},
 ]
 
