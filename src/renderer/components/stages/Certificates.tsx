f62baac2e97211614b843f66f1380897fd419e43
Timothy Gerstel <tim.gerstel@gmail.com>
2024-01-30 02:40:41 -0500

    Fix attribute
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- src/renderer/components/stages/Certificates.tsx
+++ src/renderer/components/stages/Certificates.tsx
@@ -85,9 +85,9 @@ const Certificates = () => {
         } else {
           // setConfiguration(section, newData, true);
           setLYaml((prevYaml: any) => ({
-            ...prevYaml, zowe: {...yaml.zowe, setup: {...yaml.zowe.setup, dataset: newData}}
+            ...prevYaml, zowe: {...yaml.zowe, setup: {...yaml.zowe.setup, certificate: newData}}
           }))
-          dispatch(setYaml({...yaml, zowe: {...yaml.zowe, setup: {...yaml.zowe.setup, dataset: newData}}}))
+          dispatch(setYaml({...yaml, zowe: {...yaml.zowe, setup: {...yaml.zowe.setup, certificate: newData}}}))
           setStageConfig(true, '', newData);
         }
       }
