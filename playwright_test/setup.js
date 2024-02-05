d600875eee28adf35e3f7d9b9e8734527661b472
James Struga <jstruga@rocketsoftware.com>
2024-02-05 01:41:39 -0500

    add setup steps
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>

---
+++ playwright_test/setup.js
@@ -0,0 +1,102 @@
+const zos = require('zos-node-accessor');
+const https = require('https');
+
+class Script {
+  constructor(config) {
+    this.config = config;
+  }
+
+  async mkdir(installDir) {
+    const script = `mkdir ${installDir}`;
+    return this.run(script);
+  }
+  
+  static getZoweVersion() {
+    return new Promise((resolve, reject) => {
+      https.get('https://raw.githubusercontent.com/zowe/zowe-install-packaging/v2.x/master/manifest.json.template', (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(data);
+            resolve({ status: true, details: parsedData.version });
+          } catch (error) {
+            reject({ status: false, details: { error } });
+          }
+        });
+
+      }).on('error', (error) => {
+        reject({ status: false, details: { error } });
+      });
+    });
+  }
+  
+  async download(installDir) {
+    try {
+      const { status, version } = await Script.getZoweVersion();
+      if (!status) {
+        throw new Error('Failed to retrieve Zowe version.');
+      }
+
+      const paxURL = `https://zowe.jfrog.io/zowe/list/libs-release-local/org/zowe/${version}/zowe-${version}.pax`;
+	const script = `curl -o ${installDir}/zowe.pax ${paxURL} -k`;
+      return this.run(script);
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+  
+  async unpax(installDir) {
+    const script = `cd ${installDir};\npax -ppx -rf zowe.pax;\nrm zowe.pax`;
+    return this.run(script);
+  }
+  
+  async install(installDir) {
+    try {
+      await this.mkdir(installDir);
+      await this.download(installDir);
+      await this.unpax(installDir);
+      console.log('Installation completed successfully.');
+    } catch (error) {
+      console.error('Installation failed:', error);
+    }
+  }
+
+  async run(script) {
+    const jcl = `//ZWEJOB01 JOB IZUACCT,'SYSPROG',CLASS=A,
+//         MSGLEVEL=(1,1),MSGCLASS=A
+//RUNSCRP EXEC PGM=BPXBATCH,REGION=0M
+//STDOUT DD SYSOUT=*
+//STDERR DD SYSOUT=*
+//STDPARM  DD *
+sh set -x;
+${script};
+echo "Script finished."
+/* `;
+
+    try {
+      console.log('Connecting to FTP server...');
+      const client = new zos();
+      await client.connect(this.config);
+
+      if (!client.connected) {
+        throw new Error('Failed to connect to ' + this.config.host);
+      }
+      const result = await client.submitJCL(jcl, ["STDOUT", "STDERR"]);
+      console.log('JCL submitted successfully.');
+      console.log('Result:', result);
+      return result;
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+}
+
+module.exports = Script;
+
