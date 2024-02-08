40c774d30efc8677e354329cfb02f412e14ebed0
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-02-08 17:41:03 +0530

    Last updates

--- src/renderer/components/stages/progress/progressStore.ts
+++ src/renderer/components/stages/progress/progressStore.ts
@@ -6,7 +6,7 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Copyright Contributors to the Zowe Project.
-*/
+ */
 
 import { flatten, unflatten } from 'flat';
 import { progressState } from "./progressSlice"; 
