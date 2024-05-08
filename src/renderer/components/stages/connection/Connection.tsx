69d533cd3148dfc9be57e8fc2616da727d97a570
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-05-08 19:28:55 +0530

    Updating the styling

--- src/renderer/components/stages/connection/Connection.tsx
+++ src/renderer/components/stages/connection/Connection.tsx
@@ -322,8 +322,8 @@ const FTPConnectionForm = () => {
 
         {connectionStatus && activeStepIndex>0 && <Tooltip title="Continue to Last Active Stage" arrow>
           <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'left'}}>
-            <Button sx={{color: '#858081', boxShadow: 'none', margin: '0 0 0 10px', '&:hover': { backgroundColor: 'transparent' }, '&:click': { backgroundColor: 'transparent' }}} type="submit" onClick={() => resumeProgress()}>Resume</Button>
-            <div><ArrowCircleRightIcon sx={{ color: '#858081', fontSize: '1.4rem', marginTop: '5px', marginLeft: '0', cursor: 'pointer' }} onClick={() => resumeProgress()} /></div>
+            <Button sx={{color: '#0b0b0b', boxShadow: 'none', margin: '0 0 0 10px', '&:hover': { backgroundColor: 'transparent' }, '&:click': { backgroundColor: 'transparent' }}} type="submit" onClick={() => resumeProgress()}>Resume</Button>
+            <div><ArrowCircleRightIcon sx={{ color: '#494949', fontSize: '1.4rem', marginTop: '5px', marginLeft: '0', cursor: 'pointer' }} onClick={() => resumeProgress()} /></div>
           </Box>
         </Tooltip>}
 
