0ecd66e9ea58ce600a806f5ff980ea27f82922f7
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-05-16 12:36:23 +0530

    Updating the stepper

--- src/renderer/components/common/Stepper.tsx
+++ src/renderer/components/common/Stepper.tsx
@@ -174,7 +174,7 @@ export default function HorizontalLinearStepper({stages, initialization}:{stages
 
   const getStepIcon = (error: any, stageId: number, isSubStep?: boolean, subStepId?: number) => {
     
-    if ((isSubStep && !mapAndGetSkipStatus(subStepId)) || !error || (isSubStep && getProgress(stages[stageId].subStages[subStepId].statusKey)) || (!isSubStep && getProgress(stages[stageId].statusKey))) {
+    if (!error || (isSubStep && getProgress(stages[stageId].subStages[subStepId].statusKey)) || (!isSubStep && getProgress(stages[stageId].statusKey))) {
       return <StepIcon icon={<CheckCircle sx={{ color: 'green', fontSize: '1.2rem' }} />} />;
     }
 
