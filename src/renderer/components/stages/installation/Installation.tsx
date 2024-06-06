bc7120e986258e918154644f302e6aa3fb384e55
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-06-06 16:05:26 +0530

    Reverting the chages

--- src/renderer/components/stages/installation/Installation.tsx
+++ src/renderer/components/stages/installation/Installation.tsx
@@ -247,7 +247,6 @@ const Installation = () => {
 
   const setDsInstallStageStatus = (status: boolean) => {
     dispatch(setNextStepEnabled(status));
-    dispatch(setNextStepEnabled(true));
     dispatch(setInitializationStatus(status));
     dispatch(setDatasetInstallationStatus(status));
   }
@@ -337,7 +336,6 @@ const Installation = () => {
   // True - a proceed, False - blocked
   const installProceedActions = (status: boolean) => {
     dispatch(setNextStepEnabled(status));
-    dispatch(setNextStepEnabled(true));
     dispatch(setDatasetInstallationStatus(status));
     dispatch(setInitializationStatus(status));
   }
