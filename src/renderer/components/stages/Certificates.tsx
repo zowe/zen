046af979195a7efa889888583d102894e546296d
Timothy Gerstel <tim.gerstel@gmail.com>
2024-06-20 19:24:08 -0400

    Yaml still wasnt updating properly in zen:
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- src/renderer/components/stages/Certificates.tsx
+++ src/renderer/components/stages/Certificates.tsx
@@ -170,6 +170,8 @@ const Certificates = () => {
       if(res.details.updatedYaml != undefined){
         var merge = deepMerge(res.details.updatedYaml, yaml);
         merge.zowe.certificate = res.details.updatedYaml.zowe?.certificate;
+        console.log('setting zowe.certificate to:', JSON.stringify(res.details.updatedYaml.zowe?.certificate));
+        setSetupYaml(res.details.updatedYaml.zowe?.certificate);
         window.electron.ipcRenderer.setConfig(merge);
         setLYaml(merge);
         dispatch(setYaml(merge));
