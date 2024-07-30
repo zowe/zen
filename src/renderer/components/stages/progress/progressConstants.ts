e0a01f5c0b4e6716aff345ab3cb7e00610208ebb
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-07-30 13:14:38 +0530

    Creating a separate const file

---
+++ src/renderer/components/stages/progress/progressConstants.ts
@@ -0,0 +1,130 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+import { ProgressState, PlanningState, InstallationType, ActiveState, DatasetInstallationState, InitSubStepsState, CertInitSubStepsState, PlanningValidationDetails, SkipState, InstallationArgs, DownloadUnpaxState } from "../../../../types/stateInterfaces";
+
+export const initProgressStatus: ProgressState = {
+  connectionStatus: false,
+  planningStatus: false,
+  installationTypeStatus: false,
+  downloadUnpaxStatus: false,
+  initializationStatus: false,
+  datasetInstallationStatus: false,
+  networkingStatus: false,
+  apfAuthStatus: false,
+  securityStatus: false,
+  stcsStatus: false,
+  certificateStatus: false,
+  vsamStatus: false,
+  launchConfigStatus: false,
+  reviewStatus: false,
+}
+
+export const initInstallationTypeStatus: InstallationType = {
+  installationType: 'download',
+  licenseAgreement: false,
+  userUploadedPaxPath: '',
+}
+
+export const initDownloadUnpaxStatus: DownloadUnpaxState = {
+  uploadYaml: false,
+  download: false,
+  upload: false,
+  unpax: false,
+  getExampleYaml: false,
+  getSchemas: false,
+}
+
+export const initActiveStatus: ActiveState = {
+  activeStepIndex: 0,
+  isSubStep: false,
+  activeSubStepIndex: 0,
+}
+
+export const initPlanningStageStatus: PlanningState = {
+  jobStatement: '',
+  isJobStatementValid: false,
+  isLocationValid: false,
+}
+
+export const initDatasetInstallationStatus: DatasetInstallationState = {
+  uploadYaml: false,
+  install: false,
+  initMVS: false
+}
+
+export const initApfAuthStatus: InitSubStepsState = {
+  writeYaml: false,
+  uploadYaml: false,
+  success: false
+}
+
+export const initSecurityInitStatus: InitSubStepsState = {
+  writeYaml: false,
+  uploadYaml: false,
+  success: false
+}
+
+export const initStcsInitStatus: InitSubStepsState = {
+  writeYaml: false,
+  uploadYaml: false,
+  success: false
+}
+
+export const initCertificateInitStatus: CertInitSubStepsState = {
+  writeYaml: false,
+  uploadYaml: false,
+  zweInitCertificate: false
+}
+
+export const initVsamInitStatus: InitSubStepsState = {
+  writeYaml: false,
+  uploadYaml: false,
+  success: false
+}
+
+export const initPlanningValidationDetailsStatus: PlanningValidationDetails = {
+  javaVersion: '',
+  nodeVersion: '',
+  spaceAvailableMb: '',
+  error: ''
+}
+
+export const initStepSkipStatus: SkipState = {
+  downloadUnpax: false,
+  datasetInstallation: false,
+  networking: false,
+  apfAuth: false,
+  security: false,
+  certificate: false,
+  vsam: false,
+  launchConfig: false
+}
+
+export const initInstallationArgsStatus: InstallationArgs = {
+  installationDir: '',
+    workspaceDir: '',
+    logDir:'',
+    extensionDir:'',
+    installationType: 'download',
+    userUploadedPaxPath: '',
+    downloadDir: '',
+    javaHome: '',
+    nodeHome: '',
+    setupConfig: {},
+    jobName: 'ZWE1SV',
+    jobPrefix: 'ZWE1',
+    rbacProfile: '1',
+    cookieId: '1',
+    zosmfHost: '',
+    zosmfPort: '443',
+    zosmfApplId: 'IZUDFLT',
+    dryRunMode:false
+}
