d4a6b1ea8a5c682744ba6bec32ca507cc662883f
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-08-09 15:56:49 +0530

    code-reorganizing

--- src/renderer/components/common/JsonForms.tsx
+++ src/renderer/components/common/JsonForms.tsx
@@ -14,77 +14,31 @@ import { materialRenderers, materialCells } from '@jsonforms/material-renderers'
 import { ThemeProvider } from '@mui/material/styles';
 import jsonFormTheme from '../../jsonFormsTheme';
 
-const makeUISchema = (schema: any, base: string, formData: any): any => {
-  if (!schema || !formData) {
-    return "";
-  }
-  const properties = Object.keys(schema?.properties);
+// Creates a basic input element in the UI schema
+const createControl = (scope: string) => ({
+  type: 'Control',
+  scope, // Scope specifies the JSON path
+});
 
-  // Creates a basic input element in the UI schema
-  const createControl = (scope: string) => ({
-    type: 'Control',
-    scope, // Scope specifies the JSON path
-  });
+// createGroup generates a group object, used to group together multiple form elements - can optionally include a rule (like visibility)
+const createGroup = (label: string, elements: any[], rule?: any) => ({
+  type: 'Group',
+  label: `\n${label}`,
+  elements: elements || [],
+  ...(rule && {rule}),
+});
 
-  // createGroup generates a group object, used to group together multiple form elements - can optionally include a rule (like visibility)
-  const createGroup = (label: string, elements: any[], rule?: any) => ({
-    type: 'Group',
-    label: `\n${label}`,
-    elements: elements || [],
-    ...(rule && {rule}),
-  });
+// createVerticalLayout generates a layout object that arranges its child elements vertically.
+const createVerticalLayout = (elements: any[]) => ({
+  type: 'VerticalLayout',
+  elements,
+});
 
-  // createVerticalLayout generates a layout object that arranges its child elements vertically.
-  const createVerticalLayout = (elements: any[]) => ({
-    type: 'VerticalLayout',
-    elements,
-  });
-
-  // Same as above, but arranges its child elements horizontally.
-  const createHorizontalLayout = (elements: any[]) => ({
-    type: 'HorizontalLayout',
-    elements,
-  });
-
-  // Map each property in the JSON schema to an appropriate UI element based on its type and structure.
-  const elements = properties.map((prop: any) => {
-    if (schema.properties[prop].type === 'object') {
-      // Create a group with a hide rule if patternProperties are present or conditional hiding is required.
-      if (schema.properties[prop].patternProperties || (schema.if && conditionalSchema(schema, formData, prop))) {
-        return createGroup(prop, [], {
-          effect: "HIDE",
-          condition: {}
-        });
-      }
-
-      // For objects, iterate through their properties to create controls and group them if needed.
-      const subSchema = schema.properties[prop];
-      const groupedControls: any = [];
-      let row: any = [];
-
-      if (subSchema && subSchema.properties) { // If the object has its own properties, create controls for them and organize into layouts.
-        const subProperties = Object.keys(subSchema?.properties);
-        subProperties.forEach((subProp, index) => {
-          row.push(createControl(`#/properties${base}${prop}/properties/${subProp}`)); // Create a control for each sub-property.
-
-          // Group controls into horizontal layouts for visual arrangement, resetting the row after adding.
-          if (row.length === 2 || (row.length === 1 && index === subProperties.length - 1)) {
-            groupedControls.push(createHorizontalLayout(row));
-            row = [];
-          }
-        });
-      }
-
-      return createGroup(prop, [ // Return a group containing a vertical layout of the organized controls for the object.
-        createVerticalLayout(groupedControls)
-      ]);
-    } else { // For simple properties, create a basic control.
-      return createControl(`#/properties${base}${prop}`);
-    }
-  });
-
-  return createVerticalLayout(elements); // Return whole structure
-}
+// Same as above, but arranges its child elements horizontally.
+const createHorizontalLayout = (elements: any[]) => ({
+  type: 'HorizontalLayout',
+  elements,
+});
 
 // To handle the "if", "else", and "then" in the schema
 const conditionalSchema = (schema: any, formData: any, prop: any): boolean=> {
@@ -125,6 +79,52 @@ const getDefaultFormData = (schema: any, formData: any) => {
   }
   return null;
 };
+
+const makeUISchema = (schema: any, base: string, formData: any): any => {
+  if (!schema || !formData) {
+    return "";
+  }
+  const properties = Object.keys(schema?.properties);
+
+  // Map each property in the JSON schema to an appropriate UI element based on its type and structure.
+  const elements = properties.map((prop: any) => {
+    if (schema.properties[prop].type === 'object') {
+      // Create a group with a hide rule if patternProperties are present or conditional hiding is required.
+      if (schema.properties[prop].patternProperties || (schema.if && conditionalSchema(schema, formData, prop))) {
+        return createGroup(prop, [], {
+          effect: "HIDE",
+          condition: {}
+        });
+      }
+
+      // For objects, iterate through their properties to create controls and group them if needed.
+      const subSchema = schema.properties[prop];
+      const groupedControls: any = [];
+      let row: any = [];
+
+      if (subSchema && subSchema.properties) { // If the object has its own properties, create controls for them and organize into layouts.
+        const subProperties = Object.keys(subSchema?.properties);
+        subProperties.forEach((subProp, index) => {
+          row.push(createControl(`#/properties${base}${prop}/properties/${subProp}`)); // Create a control for each sub-property.
+
+          // Group controls into horizontal layouts for visual arrangement, resetting the row after adding.
+          if (row.length === 2 || (row.length === 1 && index === subProperties.length - 1)) {
+            groupedControls.push(createHorizontalLayout(row));
+            row = [];
+          }
+        });
+      }
+
+      return createGroup(prop, [ // Return a group containing a vertical layout of the organized controls for the object.
+        createVerticalLayout(groupedControls)
+      ]);
+    } else { // For simple properties, create a basic control.
+      return createControl(`#/properties${base}${prop}`);
+    }
+  });
+
+  return createVerticalLayout(elements); // Return whole structure
+}
 
 export default function JsonForm(props: any) {
   const {schema, onChange, formData} = props;
