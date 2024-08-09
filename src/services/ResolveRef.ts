81a9bc14bd905e767db867ed0f887f2edcf30b38
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-08-09 14:45:02 +0530

    Adding the service

--- src/services/ResolveRef.ts
+++ src/services/ResolveRef.ts
@@ -3,38 +3,27 @@ export const updateSchemaReferences = (schema: any, serverCommon: any): void => 
   const traverseAndUpdate = (node: any) => {
     if (node !== null && typeof node === "object") {
       for (const key in node) {
-        console.log("-------------key: ", key);
         if (key === "$ref" && typeof node[key] === "string") {
           try {
             const refValue = resolveRef(node[key]);
-            console.log("---------refValue: ", refValue);
             Object.assign(node, refValue); 
             delete node['$ref'];
           } catch(error){
             console.error("Error resolving reference:", error.message);
           }
         } else {
-          traverseAndUpdate(node[key]); // Pass down the node[key] for recursive traversal
+          traverseAndUpdate(node[key]);
         }
       }
     }
   }
-  
+
   const resolveRef = (ref: string) => {
-    const refPath = ref.replace('#/$defs/', '').split('/');
+    const refPath = ref.split('#')[1];
     let result = serverCommon.$defs;
-    for (const part of refPath) {
-      console.log('---------------resolve[part]: ', result[part]);
-      if (result[part] !== undefined) {
-        result = result[part];
-      } else {
-        console.log('-------------error: not found: ', result[part]);
-        throw new Error(`Reference ${ref} not found in serverCommon`);
-      }
-    }
-    return result; 
+    const refObject = Object.values(result).find((obj:any) => obj.$anchor === refPath);
+    return refObject;
   }
-    
-  traverseAndUpdate(schema);
+
+  traverseAndUpdate(schema.properties.zowe.properties.setup.properties);
 }
-  
