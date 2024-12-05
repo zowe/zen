98c4612a6d3d0101c85775bea8838a6b8f7d4605
hpathak <hpathak@rocketsoftware.com>
2024-12-05 19:49:52 +0530

    removed unwanted waits
    Signed-off-by: hpathak <hpathak@rocketsoftware.com>

--- playwright_test/Tests/Certificates.spec.ts
+++ playwright_test/Tests/Certificates.spec.ts
@@ -102,98 +102,83 @@ test.describe('CertificateTab', () => {
      await electronApp.close()
    })
 
-   test('test title of page & required fields', async ({ page }) => {
-     await page.waitForTimeout(5000);
-     const title = await certificatePage.returnTitleOfCertiPage();
-     expect(title).toBe(CERTIFICATE_TITLE);
-     await expect(certificatePage.option1).toBeTruthy()
-     await expect(certificatePage.option2).toBeTruthy()
-     await expect(certificatePage.keystore_directory).toBeTruthy()
-     await expect(certificatePage.certificate_alias_name).toBeTruthy()
-     await expect(certificatePage.keystore_password).toBeTruthy()
-     await expect(certificatePage.ca_alias).toBeTruthy()
-     await expect(certificatePage.ca_password).toBeTruthy()
-     await expect(certificatePage.lock).toBeTruthy()
-     await expect(certificatePage.import_keystore).toBeTruthy()
-     await expect(certificatePage.import_password).toBeTruthy()
-     await expect(certificatePage.import_alias).toBeTruthy()
-     await expect(certificatePage.ca_common_name).toBeTruthy()
-     await expect(certificatePage.common_name).toBeTruthy()
-     await expect(certificatePage.org_unit).toBeTruthy()
-     await expect(certificatePage.org_certificate).toBeTruthy()
-     await expect(certificatePage.locality_of_cert).toBeTruthy()
-     await expect(certificatePage.state_of_cert).toBeTruthy()
-     await expect(certificatePage.country_of_cert).toBeTruthy()
-     await expect(certificatePage.validity_of_cert).toBeTruthy()
-     await expect(certificatePage.san).toBeTruthy()
-     await expect(certificatePage.import_certificate_authorities).toBeTruthy()
-     await expect(certificatePage.verify_certificates).toBeTruthy()
-   })
+ test('test title of page & required fields', async ({ page }) => {
+   const title = await certificatePage.returnTitleOfCertiPage();
+   expect(title).toBe(CERTIFICATE_TITLE);
+   await expect(certificatePage.option1).toBeTruthy()
+   await expect(certificatePage.option2).toBeTruthy()
+   await expect(certificatePage.keystore_directory).toBeTruthy()
+   await expect(certificatePage.certificate_alias_name).toBeTruthy()
+   await expect(certificatePage.keystore_password).toBeTruthy()
+   await expect(certificatePage.ca_alias).toBeTruthy()
+   await expect(certificatePage.ca_password).toBeTruthy()
+   await expect(certificatePage.lock).toBeTruthy()
+   await expect(certificatePage.import_keystore).toBeTruthy()
+   await expect(certificatePage.import_password).toBeTruthy()
+   await expect(certificatePage.import_alias).toBeTruthy()
+   await expect(certificatePage.ca_common_name).toBeTruthy()
+   await expect(certificatePage.common_name).toBeTruthy()
+   await expect(certificatePage.org_unit).toBeTruthy()
+   await expect(certificatePage.org_certificate).toBeTruthy()
+   await expect(certificatePage.locality_of_cert).toBeTruthy()
+   await expect(certificatePage.state_of_cert).toBeTruthy()
+   await expect(certificatePage.country_of_cert).toBeTruthy()
+   await expect(certificatePage.validity_of_cert).toBeTruthy()
+   await expect(certificatePage.san).toBeTruthy()
+   await expect(certificatePage.import_certificate_authorities).toBeTruthy()
+   await expect(certificatePage.verify_certificates).toBeTruthy()
+   const verifyList = await certificatePage.verifyCertificatesList();
+   expect(verifyList).toEqual(VERIFY_CERTIFICATE_LIST);
+ })
  
-  test('Test Skip certificate button is enable', async ({ page }) => {
-    const isLaunchConfigEnable = await certificatePage.is_skipCertificateButtonEnable();
-    expect(isLaunchConfigEnable).toBe(true);
-    const title = await certificatePage.click_skipCertificate();
-    expect(title).toBe(VSAM_TITLE);
+test('Test Skip certificate button is enable', async ({ page }) => {
+  const isLaunchConfigEnable = await certificatePage.is_skipCertificateButtonEnable();
+  expect(isLaunchConfigEnable).toBe(true);
+  const title = await certificatePage.click_skipCertificate();
+  expect(title).toBe(VSAM_TITLE);
+})
+
+test('Test view yaml & view job output button', async ({ page }) => {
+  await certificatePage.viewYaml()
+  await expect(certificatePage.editor_title_element).toBeTruthy();
+  await certificatePage.closeButton()
+  await certificatePage.click_viewAndSubmitJob()
+  await expect(certificatePage.editor_title_element).toBeTruthy()
+  await certificatePage.closeButton()
   })
-  test('Test view yaml button', async ({ page }) => {
-    await certificatePage.viewYaml()
-    await expect(certificatePage.editor_title_element).toBeTruthy();
-    await certificatePage.closeButton()
-    })
-   test('test all available options for verify certificates', async ({ page }) => {
-     await page.waitForTimeout(5000);
-     const verifyList = await certificatePage.verifyCertificatesList();
-     expect(verifyList).toEqual(VERIFY_CERTIFICATE_LIST);
-    })
 
-  test('test verify delete & add san button', async ({ page }) => {
-   await certificatePage.add_SanButton();
-   const isSanInputVisible1 = await certificatePage.isSanInputVisible();
-   await expect(isSanInputVisible1).toBe(true);
-   await certificatePage.add_SanDeleteButton();
-   const isVisible = await certificatePage.is_deleteAlertVisible();
-   await expect(isVisible).toBe(true);
-   const alertText = await certificatePage.Verify_alertText();
-   expect(alertText).toBe("Are you sure you want to delete the selected entry?");
-   await certificatePage.accept_declinedAlert('Yes');
-   const isSanInputVisible2 = await certificatePage.isSanInputVisible();
-   await expect(isSanInputVisible2).toBe(false);
-  });
+test('test verify delete & add san button', async ({ page }) => {
+  await certificatePage.add_SanButton();
+  const isSanInputVisible1 = await certificatePage.isSanInputVisible();
+  await expect(isSanInputVisible1).toBe(true);
+  await certificatePage.add_SanDeleteButton();
+  const isVisible = await certificatePage.is_deleteAlertVisible();
+  await expect(isVisible).toBe(true);
+  const alertText = await certificatePage.Verify_alertText();
+  expect(alertText).toBe("Are you sure you want to delete the selected entry?");
+  await certificatePage.accept_declinedAlert('Yes');
+  const isSanInputVisible2 = await certificatePage.isSanInputVisible();
+  await expect(isSanInputVisible2).toBe(false);
+ });
 
- test('Click Previous Step and verify that the Previous button is enabled.', async ({ page }) => {
-   const is_prevButtonEnable = await certificatePage.isPreviousButtonEnable();
-   expect(is_prevButtonEnable).toBe(true);
-   const title = await certificatePage.returnTitleOfPrevPage();
-   expect(title).toBe(STCS_TITLE);
-   });
+test('Click Previous Step and verify that the Previous button is enabled.', async ({ page }) => {
+  const is_prevButtonEnable = await certificatePage.isPreviousButtonEnable();
+  expect(is_prevButtonEnable).toBe(true);
+  const title = await certificatePage.returnTitleOfPrevPage();
+  expect(title).toBe(STCS_TITLE);
+});
 
-
-  test('test continue button is disable', async ({ page }) => {
-   const is_ContinueButtonDisable = await certificatePage.isContinueButtonDisable();
-   await expect(is_ContinueButtonDisable).toBe(true);
-   });
-
-
-   test('Test view job output button', async ({ page }) => {
-    await certificatePage.click_viewAndSubmitJob()
-    await expect(certificatePage.editor_title_element).toBeTruthy()
-    await certificatePage.closeButton()
-    });
-
-
-   test('Test save and close and Resume Progress', async ({ page }) => {
-     await certificatePage.fillKeystoreDir(config.ZOWE_ROOT_DIR + '/keystore');
-     await page.waitForTimeout(5000);
-     certificatePage.click_saveAndClose()
-     await titlePage.clickOnResumeProgress();
-     await connectionPage.fillConnectionDetails(config.SSH_HOST, config.SSH_PORT, config.SSH_USER, config.SSH_PASSWD);
-     await connectionPage.SubmitValidateCredential()
-     const title = await certificatePage.returnTitleOfCertiPage();
-     await expect(title).toBe(CERTIFICATE_TITLE);
-     const keystoreValue = await certificatePage.get_keystore_value();
-     await expect(keystoreValue).toBe(config.ZOWE_ROOT_DIR + '/keystore');
-   });
+test('Test save and close and Resume Progress', async ({ page }) => {
+  await certificatePage.fillKeystoreDir(config.ZOWE_ROOT_DIR + '/keystore');
+  certificatePage.click_saveAndClose()
+  await titlePage.clickOnResumeProgress();
+  await connectionPage.fillConnectionDetails(config.SSH_HOST, config.SSH_PORT, config.SSH_USER, config.SSH_PASSWD);
+  await connectionPage.SubmitValidateCredential()
+  const title = await certificatePage.returnTitleOfCertiPage();
+  await expect(title).toBe(CERTIFICATE_TITLE);
+  const keystoreValue = await certificatePage.get_keystore_value();
+  await expect(keystoreValue).toBe(config.ZOWE_ROOT_DIR + '/keystore');
+ });
 
 
 test('Test init certificates with NONSTRICT Mode & verify keystore dir created sucessfully', async ({ page }) => {
@@ -212,7 +197,8 @@ test('Test init certificates with NONSTRICT Mode & verify keystore dir created s
   await certificatePage.closeButton();
   await certificatePage.fillKeystoreDir(`${config.ZOWE_ROOT_DIR}/keystore`);
   await certificatePage.select_VerifyCert('NONSTRICT');
-
+  const isBtnDisabledBeforeInit = await certificatePage.isContinueButtonDisable();
+  await expect(isBtnDisabledBeforeInit).toBe(true);
   await certificatePage.initializeCert();
   const isWriteConfigCheckVisible = await certificatePage.isWriteConfigGreenCheckVisible();
   await expect(isWriteConfigCheckVisible).toBe(true);
@@ -241,10 +227,8 @@ test('Test init certificates with Disabled Mode and verify certificates configur
   }
   await certificatePage.closeButton();
   await certificatePage.fillKeystoreDir(`${config.ZOWE_ROOT_DIR}/keystore`);
-  await page.waitForTimeout(5000);
   await certificatePage.select_VerifyCert('DISABLED');
 
-  await page.waitForTimeout(5000);
   await certificatePage.initializeCert();
   const isWriteConfigCheckVisible = await certificatePage.isWriteConfigGreenCheckVisible();
   await expect(isWriteConfigCheckVisible).toBe(true);
@@ -270,7 +254,6 @@ test('Test init certificates with Lock keystore dir', async ({ page }) => {
   await certificatePage.fillKeystoreDir(`${config.ZOWE_ROOT_DIR}/keystore`);
   const isChecked = await certificatePage.Lock_checkbox_Ischecked();
   expect(isChecked).toBe(true); 
-  await page.waitForTimeout(5000);
   await certificatePage.select_VerifyCert('DISABLED');
   await certificatePage.initializeCert();
   const is_ContinueButtonDisable = await certificatePage.isContinueButtonDisable();
